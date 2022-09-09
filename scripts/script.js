159da1150030aa31dd5b89d9ddce1602da3d1d30
Ignas Nemura <ignas.nemura17@gmail.com>
2022-09-09 12:51:58 +0300

    Added functions and refactured styles

--- scripts/script.js
+++ scripts/script.js
@@ -1,33 +1,18 @@
 function addExpense(ID) {
-    const expensesContainer = document.createElement('div');
-    expensesContainer.className = 'expensesContainer';
-    expensesContainer.innerHTML = `
-        <div class = 'expensesContainer'>
-            <div class = 'expenses' id = 'expenses${ ID }'>
-                
-            </div>
-            <div class = 'flex totalPrice'>
-                <h3> Total </h3>
-                <h3 class = 'price'> €0.00 </h3>
-            </div>
-        </div>
-    `;
+    const expensesParentID = 'expenses' + ID;
+
+    if (!document.getElementById(expensesParentID)) {
+        createExpensesContainer(ID);
+    }
 
     const expense = document.createElement('div');
     expense.className = 'expense';
     expense.innerHTML = `
-        <div class = 'flex'>
+        <div class = 'grid'>
             <input type = 'text' placeholder = 'Expense name'>
-            <input type = 'number' min = '0.00' step = '0.01' placeholder = '€0.00' onkeypress = 'calculateTotalPrice()' onkeyup = 'calculateTotalPrice()' onkeydown = 'calculateTotalPrice()'/>
+            <input type = 'number' name = 'expense${ ID }' id = 'name = 'expense${ ID }' min = '0.00' step = '0.01' placeholder = '€0.00' onblur = 'calculateExpenses(${ ID })' />
         </div>
     `;
-
-    const receiptContainerParentID = 'receiptContainer' + ID;
-    const expensesParentID = 'expenses' + ID;
-
-    if (!document.getElementById(expensesParentID)) {
-        document.getElementById(receiptContainerParentID).appendChild(expensesContainer);
-    }
 
     document.getElementById(expensesParentID).appendChild(expense);
 }
@@ -40,23 +25,73 @@ function addReceipt() {
     const header = `
         <div class = 'header flex'>
             <div class = 'selections'>
-                <select id = 'expenses' name = 'expenses'>
-                    <option selected = 'selected'> Select: </option>
+                <select id = 'categories${ ID }' name = 'categories${ ID }'>
+                    <option value = '' disabled selected> Select </option>
                     <option value = 'food'> Food </option>
                     <option value = 'houseware'> Houseware </option>
                     <option value = 'entertainment'> Entertainment </option>
                 </select>
             </div> 
-            <button id = 'receiptButton${ ID }' class = 'button expenseButton' type = 'button' onclick = 'addExpense(${ ID })'> Add expense </button>
+            <button id = 'receiptButton${ ID }' class = 'button expenseButton' type = 'button' onclick = 'checkIfCategorySelected(${ ID })'> Add expense </button>
         </div>
     `;
 
     receiptContainer.innerHTML = header;
-
     receiptContainer.id = 'receiptContainer' + ID;
+    
     document.getElementsByClassName('container')[0].appendChild(receiptContainer);
 }
 
-function calculateTotalPrice() {
-    
+function createExpensesContainer(ID) {
+    const expensesContainer = document.createElement('div');
+    expensesContainer.className = 'expensesContainer';
+    expensesContainer.innerHTML = `
+        <div class = 'expenses' id = 'expenses${ ID }'>
+            
+        </div>
+        <div class = 'flex totalPrice'>
+            <h3> Total </h3>
+            <h3 name = 'totalExpensesPrice${ ID }' id = 'totalExpensesPrice${ ID }' class = 'price'> € 0.00 </h3>
+        </div>
+    `;
+
+    const receiptContainerParentID = 'receiptContainer' + ID;
+    document.getElementById(receiptContainerParentID).appendChild(expensesContainer);
+}
+
+function checkIfCategorySelected(ID) {
+    var categories = document.getElementById('categories' + ID);
+    var selectedValue = categories.options[categories.selectedIndex].value;
+    if (!selectedValue || selectedValue == '') {
+        alert('Please select the receipt category!');
+    } else {
+        addExpense(ID);
+    }
+}
+
+function calculateExpenses(ID) {
+    var inputs = document.getElementsByName('expense' + ID);
+    var total = calculateSum(inputs, 'expenses');
+
+    document.getElementById('totalExpensesPrice' + ID).innerHTML = total.toFixed(2);
+    calculateReceipts();
+}
+
+function calculateReceipts() {
+    var containersCount = document.getElementsByClassName('receiptContainer');
+    var total = calculateSum(containersCount, 'receipts');
+
+    document.getElementById('totalExpenses').innerHTML = total.toFixed(2);
+}
+
+function calculateSum(inputs, type) {
+    var total = 0;
+
+    for (var i = 0; i < inputs.length; i++) {
+        var element = ((type == 'expenses') ? inputs[i].value : document.getElementById('totalExpensesPrice' + (i + 1)).innerHTML);
+        if (parseFloat(element))
+        total += parseFloat(element);
+    }
+
+    return total;
 }
